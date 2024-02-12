2e2ad19b3cb7bcca79d32ccfcce05e70084879cc
Y0SDan <yosd4n@gmail.com>
2024-02-12 09:46:03 -0600

    cambie las variables

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,7 +12,7 @@ import { FaqComponent } from './components/faq/faq.component';
 const routes: Routes = [
   {
     path:"",
-    redirectTo:"/conecta",
+    redirectTo:"/home",
     pathMatch:"full"
   },
   {
