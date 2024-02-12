4214eb91b7ba6b342cae701d0a076c0563bf4ad9
Y0SDan <yosd4n@gmail.com>
2024-02-12 10:30:37 -0600

    intento 3 de hace 2 semanas

--- src/app/environments/environement.prod.ts
+++ src/app/environments/environement.prod.ts
@@ -1,5 +1,5 @@
 // environment.prod.ts
 export const environment = {
   production: true,
-  API_URI: 'https://hifas-de-grana.onrender.com/api/',
-  };
+  apiUrl: 'https://hifas-de-grana.onrender.com/api', // URL del servidor en producciónd
+};
