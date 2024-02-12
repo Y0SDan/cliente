8fb94f2523889ab7f8600378a6be12c71c0e56c2
Y0SDan <yosd4n@gmail.com>
2024-02-12 11:30:21 -0600

    sere curioso

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -16,6 +16,11 @@ const routes: Routes = [
     pathMatch:"full"
   },
   {
+    path:"login",
+    redirectTo:"/login",
+    pathMatch:"full"
+  },
+  {
     path:"home",
     component:HomeComponent,
   },
