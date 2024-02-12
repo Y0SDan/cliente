b456bc79e5c1331f3a131f19ad2cee06dcf9dd25
Y0SDan <yosd4n@gmail.com>
2024-02-12 10:59:37 -0600

    es que yo ya no entiendo nada

--- src/app/environments/environement.prod.ts
+++ src/app/environments/environement.prod.ts
@@ -1,5 +1,5 @@
 // environment.prod.ts
 export const environment = {
   production: true,
-  apiUrl: 'https://hifas-de-grana.onrender.com:3000/api/', // URL del servidor en producción
+  apiUrl: 'https://hifas-de-grana.onrender.com/api/', // URL del servidor en producción
 };
