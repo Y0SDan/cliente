80038f80ab2a5536ee08342a57faaf7e8c6a188e
Y0SDan <yosd4n@gmail.com>
2024-02-12 09:55:14 -0600

    pleaseeee

--- src/app/environments/environement.prod.ts
+++ src/app/environments/environement.prod.ts
@@ -1,5 +1,5 @@
 // environment.prod.ts
 export const environment = {
-    production: true,
-    API_URI: "https://hifas-de-grana.onrender.com:4200/", // URL del servidor en producciónd
+  production: true,
+  apiUrl: 'https://proyectoserver.onrender.com//api', // UR
   };
